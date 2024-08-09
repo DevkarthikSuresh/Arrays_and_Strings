00840d234f015e19988e8fa09c3d87507696dde8
DevkarthikSuresh <devkarthik300@gmail.com>
2024-08-09 12:04:34 +0530

    Create printing_array.cpp

---
+++ printing_array.cpp
@@ -0,0 +1,24 @@
+// Name: Devkarthik Suresh
+// PRN: 23070123045
+// Aim: Printing elemts of a user defined array.
+#include <iostream>
+using namespace std;
+int main() {
+ 
+ int x[6] = {19,10,8,6,8};
+ for(int i = 0;i<5;i++)
+ {cout << x[i];
+    cout<< endl;
+ }
+
+
+    return 0;
+}
+
+/* 
+19
+10
+8
+6
+8
+*/
